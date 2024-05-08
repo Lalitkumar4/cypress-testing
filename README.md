7ab727a78b96a118082a807e7183162e5381d705
Lalitkumar4 <64465383+Lalitkumar4@users.noreply.github.com>
2024-05-08 15:13:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cypress Testing
+
+These projects aimed to practice `Cypress` testing.
+
+Note:- I use these projects to practice Cypress testing; I don't create them.
---
+++ project-1/src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css"
+import SideNav from "./components/SideNav/SideNav"
+import Habit from "./components/Habit/Habit"
+import Accomplishment from "./components/Accomplishment/Accomplishment"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Elements from "./components/Elements/Elements"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <div className="App-container">
+          <SideNav />
+          <Routes>
+            <Route path="/habits" element={<Habit />} />
+            <Route path="/accomplishments" element={<Accomplishment />} />
+            {/* <Route path="/rewards" element={<Rewards/>} /> */}
+            <Route path="/elements" element={<Elements />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
